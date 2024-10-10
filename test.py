14bd6420054bd1814737e73316ded366dc6525ce
mahmud_cell <sadikaims02@gmail.com>
2024-10-10 15:15:33 +0200

    lose

---
+++ test.py
@@ -0,0 +1,8 @@
+fin_expense = 200
+extype = str("")
+pre_expense = 300
+extype1 = str("")
+eco_expense = 150
+extype2 = str("")
+result = "Hi, we have some different expense in this month. For" + f'{extype}' + "we have" + f' {fin_expense} , ' + "for" + f'{extype1}' +"we have" + f'{pre_expense} ,  ' + "for" + f'{extype2}' +"we have" + f'{eco_expense}.'
+print(result)
